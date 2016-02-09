46c4fba1ff2519cb068d24d9a16392432bafd4b7
Andrew Hampton <drew@firma8.com>
2016-02-09 14:37:19 -0500

    Changing how I get the api key

--- hammerspoon/.hammerspoon/menubar.lua
+++ hammerspoon/.hammerspoon/menubar.lua
@@ -3,18 +3,32 @@ local menubar = {}
 
 local weatherBar = hs.menubar.new()
 
-local apiKey = os.getenv('FORECAST_IO_API_KEY')
+local apiKey = nil
 
 function menubar.init()
-   updateWeatherBar()
-
-   hs.timer.doEvery(15*60, updateWeatherBar)
+   if file_exists("forecast_io_api_key.lua") then
+      apiKey = require "forecast_io_api_key"
+      updateWeatherBar()
+      hs.timer.doEvery(15*60, updateWeatherBar)
+   else
+      print('No forecast.io api key found')
+   end
 end
 
 function updateWeatherBar()
    local w = weather.weather(apiKey)
    weatherBar:setTitle(w.icon .. w.temp .. '/' .. w.max .. '/' .. w.min)
    weatherBar:setMenu(w.toolbarDetails)
+end
+
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then
+      io.close(f)
+      return true
+   else
+      return false
+   end
 end
 
 return menubar
