64f37daada943c7449e311353a97d7f5ab2cd6bb
Andrew Hampton <andrewwh@gmail.com>
2022-03-14 21:56:00 -0400

    hammerspoon: catch up

--- hammerspoon/.hammerspoon/hyperterm.lua
+++ hammerspoon/.hammerspoon/hyperterm.lua
@@ -3,41 +3,57 @@ local hyperterm = {}
 local logger = hs.logger.new("ht", "debug")
 
 function hyperterm.init()
-   hyperterm.terminalModeHandler = hs.eventtap.new({hs.eventtap.event.types.keyDown}, terminalMode)
+   -- hyperterm.terminalModeHandler = hs.eventtap.new({hs.eventtap.event.types.keyDown}, terminalMode)
    hyperterm.normalModeHandler = hs.eventtap.new({hs.eventtap.event.types.keyDown}, normalMode)
+   hyperterm.normalModeHandler:start()
    hyperterm.watcher = hs.application.watcher.new(hypertermActivationHandler)
    hyperterm.watcher:start()
+   -- log("key modifier rewriting started")
+
+
+   tap = hs.eventtap.new({
+     hs.eventtap.event.types.nullEvent,
+     hs.eventtap.event.types.keyDown,
+     hs.eventtap.event.types.keyUp,
+     hs.eventtap.event.types.flagsChanged,
+   }, massEventLogger)
+   tap:start()
+end
+
+function massEventLogger(event)
+  log("mass event")
+  log(hs.inspect.inspect(event))
 end
 
 function hypertermActivationHandler(appName, eventType, app)
-   log(appName)
-   log(eventType)
+   -- log(appName)
+   -- log(eventType)
    if appName == "HyperTerm" or appName == "Emacs" or appName == "Terminal" or appName == "kitty" then
       if eventType == hs.application.watcher.activated then
-         log("terminal mode activated")
-         hyperterm.terminalModeHandler:start()
-         -- hyperterm.normalModeHandler:stop()
+         -- log("terminal mode activated")
+         -- hyperterm.terminalModeHandler:start()
+         hyperterm.normalModeHandler:stop()
       elseif eventType == hs.application.watcher.deactivated then
-         log("normal mode activated")
+         -- log("normal mode activated")
          -- hyperterm.terminalModeHandler:stop()
          hyperterm.normalModeHandler:start()
       end
    end
 end
 
-function terminalMode(event)
-   log('terminal key press')
-   local terminalMap = {
-      shift = { shift = true },
-      fn    = { fn = true },
-      cmd   = { cmd = true },
-      ctrl  = { ctrl = true },
-      alt   = { alt = true },
-      includeMetaEvent = true
-   }
+-- function terminalMode(event)
+--    log('terminal key press')
+--    local terminalMap = {
+--       shift = { shift = true },
+--       fn    = { fn = true },
+--       cmd   = { cmd = true },
+--       ctrl  = { ctrl = true },
+--       alt   = { alt = true },
+--       includeMetaEvent = true
+--    }
 
-   return translate(event, terminalMap)
-end
+--    return translate(event, terminalMap)
+-- end
 
 function normalMode(event)
    log('normal key press')
@@ -53,87 +69,42 @@ function normalMode(event)
    return translate(event, normalMap)
 end
 
--- function normalMode(event)
---    local flags = event:getFlags()
---    local newFlags = {}
---    local returnEvents = {}
---    log('normal key press')
-
---    if flags.ctrl and flags.shift and flags.cmd then
---       return false, {}
---    end
-
---    newFlags.shift = flags.shift
---    newFlags.fn = flags.fn
-
---    merge(flags.cmd, newFlags, { alt = true })
---    log('cmd')
---    log(hs.inspect.inspect(newFlags))
-
---    merge(flags.ctrl, newFlags, { cmd = true })
---    log('ctrl')
---    log(hs.inspect.inspect(newFlags))
-
---    merge(flags.alt, newFlags, { ctrl = true })
---    log('alt')
---    log(hs.inspect.inspect(newFlags))
-
---    event:setFlags(newFlags)
-
---    if false and flags.ctrl and map.includeMetaEvent then
---       returnEvents = {hs.eventtap.event.newKeyEvent({}, "escape", true), hs.eventtap.event.newKeyEvent({}, "escape", false), event}
---    else
---       returnEvents = {event}
---    end
-
---    log('old')
---    log(hs.inspect.inspect(flags))
---    log('new')
---    log(hs.inspect.inspect(newFlags))
---    log('event')
---    log(event)
---    log('\n\n\n------------------------------------------')
---    return true, returnEvents
--- end
-
 function translate(event, map)
    local flags = event:getFlags()
    local newFlags = {}
 
-   log('tranlating')
-   log(hs.inspect.inspect(flags))
+   -- log('tranlating')
+   -- log(hs.inspect.inspect(flags))
    if flags.ctrl and flags.shift and flags.cmd then
       return false, {}
    end
 
    merge(flags.shift, newFlags, map.shift)
-   log('shift')
-   log(hs.inspect.inspect(newFlags))
+   -- log('shift')
+   -- log(hs.inspect.inspect(newFlags))
 
    merge(flags.fn, newFlags, map.fn)
-   log('fn')
-   log(hs.inspect.inspect(newFlags))
+   -- log('fn')
+   -- log(hs.inspect.inspect(newFlags))
 
    merge(flags.cmd, newFlags, map.cmd)
-   log('cmd')
-   log(hs.inspect.inspect(newFlags))
+   -- log('cmd')
+   -- log(hs.inspect.inspect(newFlags))
 
    merge(flags.ctrl, newFlags, map.ctrl)
-   log('ctrl')
-   log(hs.inspect.inspect(newFlags))
+   -- log('ctrl')
+   -- log(hs.inspect.inspect(newFlags))
 
    merge(flags.alt, newFlags, map.alt)
-   log('alt')
-   log(hs.inspect.inspect(newFlags))
+   -- log('alt')
+   -- log(hs.inspect.inspect(newFlags))
 
    event:setFlags(newFlags)
 
-   log('old')
+   log('from: ')
    log(hs.inspect.inspect(flags))
-   log('new')
+   log('to: ')
    log(hs.inspect.inspect(newFlags))
-   log('event')
-   log(event)
    log('\n\n\n------------------------------------------')
    return true, {event}
 end
@@ -147,7 +118,7 @@ function merge(enabled, target, source)
 end
 
 function log(msg)
-   logger:d(msg)
+   --logger:d(msg)
 end
 
 return hyperterm
