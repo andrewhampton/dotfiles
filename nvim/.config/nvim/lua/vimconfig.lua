fde20184f4bf102ad8be07a909752466858da83c
Andrew Hampton <andrewwh@gmail.com>
2022-04-09 11:08:04 -0400

    nvim: use the new augroup api

--- nvim/.config/nvim/lua/vimconfig.lua
+++ nvim/.config/nvim/lua/vimconfig.lua
@@ -1,6 +1,7 @@
 local o = vim.o   -- Global options
 local wo = vim.wo -- Window options
 local bo = vim.bo -- Buffer options
+local api = vim.api -- Vim API
 
 o.autochdir = true
 o.hidden = true
@@ -19,21 +20,19 @@ o.signcolumn = 'yes'
 
 vim.cmd('hi SpellBad gui=undercurl')
 
--- Auto-rebalance windows on resize
-vim.cmd([[
-augroup autoResize
-  autocmd!
-  autocmd VimResized * wincmd =
-augroup END
-]])
+local resizeGroup = api.nvim_create_augroup("autoResize", { clear = true })
+api.nvim_create_autocmd("VimResized", {
+  command = "wincmd =",
+  group = resizeGroup
+})
 
 -- Special config for text files
-vim.cmd([[
-augroup textBuffers
-  autocmd!
-  autocmd FileType markdown,text setlocal wrap spell list
-augroup end
-]])
+local textBuffersGroup = api.nvim_create_augroup("textBuffers", { clear = true })
+api.nvim_create_autocmd("FileType", {
+  command = "setlocal wrap spell list",
+  pattern = "markdown,text",
+  group = textBuffersGroup
+})
 
 -- Add the Jump command for git jump
 vim.cmd([[
