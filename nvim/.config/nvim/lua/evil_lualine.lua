fecc8d22eb8941afec55d9d8b29e22a4b412a175
Andrew Hampton <andrewwh@gmail.com>
2025-01-10 09:18:54 -0500

    fix(nvim): update deprecated api call

--- nvim/.config/nvim/lua/evil_lualine.lua
+++ nvim/.config/nvim/lua/evil_lualine.lua
@@ -130,7 +130,7 @@ ins_right {
   function()
     local msg = ''
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then return msg end
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
